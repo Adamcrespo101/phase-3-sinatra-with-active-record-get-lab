182dda5f62ba30999959960f0e89992cdf0303be
Adam Crespo <adam.crespo101@outlook.com>
2022-04-16 13:42:44 -0400

    commit tree 871ff51112ef114944ab94f9f8776e6a69477feb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < Sinatra::Base
-
+ 
+  set :default_content_type, 'application/json'
   # add routes
+
+  get '/bakeries' do 
+    bakeries = Bakery.all 
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do 
+    bakery = Bakery.find_by(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do 
+  
+    by_price = BakedGood.all.order(:price).reverse
+    by_price.to_json
+  
+  end
+
+  get '/baked_goods/most_expensive' do 
+      expensive = BakedGood.all.max_by{|good| good.price}
+      expensive.to_json
+  end
 
 end
